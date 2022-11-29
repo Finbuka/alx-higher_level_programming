f55d76dfc322383da8343cfe5059a499fd9339b4
Finbuka <ebukajoe100@gmail.com>
2022-11-29 16:33:35 +0100

    jquary

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,5 @@
 $(function () {
-    let name = $.get('https://swapi-api.hbtn.io/api/people/5/?format=json',function(name){
+    $.get('https://swapi-api.hbtn.io/api/people/5/?format=json',function(name){
         $('DIV#character').text(name.name)
-
     })
 })
