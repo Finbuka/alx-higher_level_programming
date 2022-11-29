aca5d552f20bb95966517e899889a9a7f595e094
Finbuka <ebukajoe100@gmail.com>
2022-11-29 16:07:54 +0100

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('DIV#toggle_header').click(function (){
+        if($('header').hasClass('green')){
+            $('header').removeClass('green')
+            $('header').addClass('red')
+        }else{
+            $('header').removeClass('red')
+            $('header').addClass('green')
+        }
+
+    })
+})
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+$(function () {
+    let name = $.get('https://swapi-api.hbtn.io/api/people/5/?format=json',function(name){
+        $('DIV#character').text(name.name)
+
+    })
+})
